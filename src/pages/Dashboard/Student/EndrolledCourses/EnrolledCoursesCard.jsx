f2b9b6b77de537a3806ae489bcb610fcb7a3d5fe
AlaminHasanPro <alaminhasanpro@gmail.com>
2024-01-15 22:53:13 +0600

    Long asset shorte

--- src/pages/Dashboard/Student/EndrolledCourses/EnrolledCoursesCard.jsx
+++ src/pages/Dashboard/Student/EndrolledCourses/EnrolledCoursesCard.jsx
@@ -9,7 +9,6 @@ import { AuthContext } from "../../../../Contexts/AuthProvider";
 const EnrolledCoursesCard = ({ items }) => {
   const { user } = useContext(AuthContext);
   const [course, setCourse] = useState({});
-
   useEffect(() => {
     axios
       .get(`http://localhost:3000/courses/${items}`)
