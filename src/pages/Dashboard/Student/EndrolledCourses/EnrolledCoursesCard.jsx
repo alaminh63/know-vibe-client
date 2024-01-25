39bbfa63adf2dde3494ceef0a34cdbe9b36edf2a
AlaminHasanPro <alaminhasanpro@gmail.com>
2024-01-25 22:26:29 +0600

    design changed from code pan

--- src/pages/Dashboard/Student/EndrolledCourses/EnrolledCoursesCard.jsx
+++ src/pages/Dashboard/Student/EndrolledCourses/EnrolledCoursesCard.jsx
@@ -11,6 +11,7 @@ const EnrolledCoursesCard = ({ items }) => {
   const { user } = useContext(AuthContext);
   const [course, setCourse] = useState({});
   
+  
   useEffect(() => {
     axios
       .get(`http://localhost:3000/courses/${items}`)
