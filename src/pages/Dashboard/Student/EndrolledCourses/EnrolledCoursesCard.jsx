0e4d62b8bbcbe08ae823b02ef9651a67d2af8fe0
AlaminHasanPro <alaminhasanpro@gmail.com>
2024-02-02 21:24:34 +0600

    error fixed and updated

--- src/pages/Dashboard/Student/EndrolledCourses/EnrolledCoursesCard.jsx
+++ src/pages/Dashboard/Student/EndrolledCourses/EnrolledCoursesCard.jsx
@@ -8,6 +8,7 @@ import { AuthContext } from "../../../../Contexts/AuthProvider";
 const EnrolledCoursesCard = ({ items }) => {
   const { user } = useContext(AuthContext);
 
+
   
   const [course, setCourse] = useState({});
   useEffect(() => {
